dd6cc7a3015c3272171ebcbb58992b860cbb1971
Berke Bozacı <bbozaci20@ku.edu.tr>
2023-04-20 02:16:06 +0300

    Adding more panels and testing the each of them

--- src/main/java/app/ui/views/playermenu/PlayerMenuFrame.java
+++ src/main/java/app/ui/views/playermenu/PlayerMenuFrame.java
@@ -15,9 +15,14 @@ import java.awt.event.ActionListener;
 
 public class PlayerMenuFrame extends BaseJFrame implements ActionListener {
 
-    private PlayerPanel playerPanel;
-    private PlayerPanelController playerPanelController;
+    private PlayerPanel playerPanel1;
+    private PlayerPanelController playerPanelController1;
 
+    private PlayerPanel playerPanel2;
+    private PlayerPanelController playerPanelController2;
+
+    private final JLabel welcomeLabel;
+    private final JLabel contextLabel;
     private final JButton playButton;
     JOptionPane optionPane;
 
@@ -26,6 +31,11 @@ public class PlayerMenuFrame extends BaseJFrame implements ActionListener {
         super(title, size, location);
         this.setLayout(null);
         initilizeComponents();
+
+        welcomeLabel = new JLabel("Welcome to ConKUeror");
+        contextLabel = new JLabel("Please select players and enter names");
+        welcomeLabel.setBounds(570, 20, 200, 30);
+        contextLabel.setBounds(520, 50, 500, 30);
 
         playButton = new JButton("Play");
         playButton.setBounds(1150, 620, 80, 30);
@@ -39,8 +49,12 @@ public class PlayerMenuFrame extends BaseJFrame implements ActionListener {
 
     @Override
     public void initilizeComponents() {
-        playerPanel = new PlayerPanel(50, 100);
-        playerPanelController = new PlayerPanelController(playerPanel);
+        playerPanel1 = new PlayerPanel(50, 100);
+        playerPanelController1 = new PlayerPanelController(playerPanel1);
+
+        playerPanel2 = new PlayerPanel(50, 250);
+        playerPanelController2 = new PlayerPanelController(playerPanel2);
+
     }
 
     @Override
@@ -55,12 +69,15 @@ public class PlayerMenuFrame extends BaseJFrame implements ActionListener {
 
     @Override
     public void addComponents() {
-        this.add(playerPanel);
+        this.add(playerPanel1);
+        this.add(playerPanel2);
         this.add(playButton);
+        this.add(welcomeLabel);
+        this.add(contextLabel);
     }
 
     private JFrame getRootFrame() {
-        return (JFrame) SwingUtilities.getWindowAncestor(playerPanel);
+        return (JFrame) SwingUtilities.getWindowAncestor(playerPanel1);
     }
 
     @Override
@@ -71,7 +88,8 @@ public class PlayerMenuFrame extends BaseJFrame implements ActionListener {
                 JFrame rootFrame = getRootFrame();
                 Point location = rootFrame.getLocation();
                 rootFrame.dispose();
-                PlayerService.createPlayer(playerPanelController.newNames);
+                PlayerService.createPlayer(playerPanelController1.newNames);
+                PlayerService.createPlayer(playerPanelController2.newNames);
                 new GameFrame(AppConfig.title, AppConfig.appSize, location);
                 this.setVisible(false);
             }
