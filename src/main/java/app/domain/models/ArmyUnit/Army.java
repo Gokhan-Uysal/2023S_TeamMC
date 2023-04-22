95adc825fc60256268e593c0008cc8cd4d220878
EfeErtugErdem <efeerdem20@ku.edu.tr>
2023-04-23 02:04:04 +0300

    Changed the army unit container to a hashmap and implemented the transferArmyUnit method.

--- src/main/java/app/domain/models/ArmyUnit/Army.java
+++ src/main/java/app/domain/models/ArmyUnit/Army.java
@@ -1,26 +1,40 @@
 package app.domain.models.ArmyUnit;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Army {
-    protected ArrayList<ArmyUnit> armyContainer = new ArrayList<>();
+    protected HashMap<ArmyUnitType, ArrayList<ArmyUnit>> armyContainer = new HashMap<>();
     protected ArmyUnitFactory armyUnitFactory = new ArmyUnitFactory();
+
+    public Army(){
+        this.armyContainer.put(ArmyUnitType.Artillery, new ArrayList<>());
+        this.armyContainer.put(ArmyUnitType.Chivalry, new ArrayList<>());
+        this.armyContainer.put(ArmyUnitType.Infantry, new ArrayList<>());
+    }
 
     public void addArmyUnits(ArmyUnitType type, int amount){
         for (int i = 0; i < amount; i++){
-            this.armyContainer.add(armyUnitFactory.create(type));
+            this.armyContainer.get(type).add(armyUnitFactory.create(type));
         }
     }
+    public void addArmyUnits(ArmyUnitType type, ArrayList<ArmyUnit> armyUnitList){
+        this.armyContainer.get(type).addAll(armyUnitList);
+    }
 
-    public ArrayList<ArmyUnit> getArmyUnit(int amount){
+    public ArrayList<ArmyUnit> getArmyUnits(ArmyUnitType type, int amount){
         ArrayList<ArmyUnit> aUnits = new ArrayList<>();
         for (int i = 0; i < amount; i++){
-            aUnits.add(this.armyContainer.remove(0));
+            aUnits.add(this.armyContainer.get(type).remove(0));
         }
         return aUnits;
     }
 
-    public void transferArmyUnits(){}
+    public void transferArmyUnits(Army otherArmy, ArmyUnitType type, int amount){
+        for (int i = 0; i < amount; i++){
+            otherArmy.addArmyUnits(type, this.getArmyUnits(type, amount));
+        }
+    }
 
     public void tradeArmyUnits(){}
 }
