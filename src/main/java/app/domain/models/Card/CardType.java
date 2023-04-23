d26b42f350162d914011be62b65ddafae89af67f
EfeErtugErdem <efeerdem20@ku.edu.tr>
2023-04-24 02:11:08 +0300

    Started implementing the deck class and implemented an enum class for card types.

---
+++ src/main/java/app/domain/models/Card/CardType.java
@@ -0,0 +1,9 @@
+package app.domain.models.Card;
+
+public enum CardType {
+    Infantry,
+    Chivalry,
+    Artillery,
+    Territory,
+    Chance
+}
---
+++ src/main/java/app/domain/models/Card/Deck.java
@@ -0,0 +1,14 @@
+package app.domain.models.Card;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Deck {
+
+    public HashMap<CardType, ArrayList<BaseCard>> deckContainer;
+
+    public Deck() {}
+    //public BaseCard drawCard(){}
+    public void addCard(){}
+    public void shuffle(){}
+}
