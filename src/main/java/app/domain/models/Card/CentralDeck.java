76c617102f612c9fe60c49cadcd2e87e5c4ada23
EfeErtugErdem <efeerdem20@ku.edu.tr>
2023-05-01 15:23:26 +0300

    Changed the implementation of the deck object.

--- src/main/java/app/domain/models/Card/CentralDeck.java
+++ src/main/java/app/domain/models/Card/CentralDeck.java
@@ -1,5 +1,6 @@
 package app.domain.models.Card;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -12,19 +13,19 @@ public class CentralDeck implements Deck {
 
     public CentralDeck() {
         this.cardContainer.put(CardType.Army, new ArrayList<>());
-        this.cardContainer.put(CardType.Territory, new ArrayList<>());
         this.cardContainer.put(CardType.Chance, new ArrayList<>());
     }
 
-    @Override
-    public void addCards(CardType type, BaseCard baseCard) {
-        BaseCard addedCard = cardFactory.create(type, baseCard);
-        if (type.equals(CardType.Artillery) || type.equals(CardType.Infantry) || type.equals(CardType.Cavalry)
-                || type.equals(CardType.Territory)) {
-            this.cardContainer.get(CardType.Army).add(addedCard);
-        } else if (type.equals(CardType.Chance)) {
-            this.cardContainer.get(CardType.Chance).add(addedCard);
-        }
+    public void addArmyCards(CardType type, String description, ImageIcon imageIcon) {
+        this.cardContainer.get(type).add(this.cardFactory.createArmyCard(type, description, imageIcon));
+    }
+
+    public void addTerritoryCards(String description, ImageIcon imageIcon, int territoryId){
+        this.cardContainer.get(CardType.Army).add(this.cardFactory.createTerritoryCard(description, imageIcon, territoryId));
+    }
+
+    public void addChanceCards(String description, ImageIcon imageIcon){
+        this.cardContainer.get(CardType.Chance).add(this.cardFactory.createChanceCard(description, imageIcon));
     }
 
     @Override
