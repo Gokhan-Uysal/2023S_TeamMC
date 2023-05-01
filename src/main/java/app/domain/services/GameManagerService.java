48f64f23df5d79225c77abd9ef025e6163d5ea0b
Berke Bozacı <bbozaci20@ku.edu.tr>
2023-05-01 17:45:23 +0300

    Game state enum added and imported to GameManagerService

--- src/main/java/app/domain/services/GameManagerService.java
+++ src/main/java/app/domain/services/GameManagerService.java
@@ -1,21 +1,39 @@
 package app.domain.services;
 
+import app.domain.models.game.GameState;
 import app.domain.services.base.BasePublisher;
 
-public class GameManagerService extends BasePublisher<Integer> {
+public class GameManagerService extends BasePublisher<GameState> {
     private static GameManagerService _instance;
-    private static Integer _gameState;
+    private GameState gameState;
 
     private GameManagerService() {
-        super(_gameState);
-        setState(0);
+        super(GameState.INITIALIZATION);
+        setState(GameState.INITIALIZATION);
+        initializeGame();
     }
 
     public static GameManagerService getInstance() {
         if (_instance == null) {
-            GameManagerService._instance = new GameManagerService();
+            _instance = new GameManagerService();
         }
-        return GameManagerService._instance;
+        return _instance;
     }
+
+    private void initializeGame() {
+        // Initialize your game here.
+        // ...
+        // After initialization, set the game state to REPLACEMENT_PHASE.
+        setState(GameState.REPLACEMENT_PHASE);
+        notifySubscribers(GameState.REPLACEMENT_PHASE);
+    }
+
+    public void updateGameState(GameState newState) {
+        // Update the game state and perform the corresponding actions.
+        setState(newState);
+        notifySubscribers(newState);
+    }
+
+    // Add methods for handling game logic here.
 
 }
