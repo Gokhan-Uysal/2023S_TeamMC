bf085ceac52310f10183842f70ad60605e50fd23
EfeErtugErdem <efeerdem20@ku.edu.tr>
2023-05-02 16:48:46 +0300

    Implemented the player class.

--- src/main/java/app/domain/models/Player/Player.java
+++ src/main/java/app/domain/models/Player/Player.java
@@ -1,11 +1,41 @@
 package app.domain.models.Player;
 
+import app.domain.models.ArmyUnit.Army;
+import app.domain.models.Card.Deck;
+
+import java.util.ArrayList;
+
 public class Player {
-    public String username;
-    public Integer id;
+    private String username;
+    private Integer id;
+    private Army playerArmy;
+    private Deck playerDeck;
+    private ArrayList<Integer> territoryList;
 
     public Player(String username, Integer idx){
         this.username = username;
         this.id = idx;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Army getPlayerArmy() {
+        return playerArmy;
+    }
+
+    public Deck getPlayerDeck() {
+        return playerDeck;
+    }
+
+    public ArrayList<Integer> getTerritoryList() {
+        return territoryList;
+    }
+
+
 }
