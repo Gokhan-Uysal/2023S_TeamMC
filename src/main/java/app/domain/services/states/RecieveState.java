4198d1cfe535f27db5efae601563c2634f0b0740
EfeErtugErdem <efeerdem20@ku.edu.tr>
2023-05-17 15:30:18 +0300

    Implemented the business logic for receive state.

--- src/main/java/app/domain/services/states/RecieveState.java
+++ src/main/java/app/domain/services/states/RecieveState.java
@@ -1,11 +1,36 @@
 package app.domain.services.states;
 
+import app.domain.models.army.ArmyUnitType;
+import app.domain.models.game.map.Territory;
+import app.domain.services.PlayerService;
 import app.domain.services.map.MapService;
 
 public class RecieveState {
     private MapService _mapService;
+    private PlayerService _playerService;
 
     public RecieveState() {
         _mapService = MapService.getInstance();
+        _playerService = PlayerService.getInstance();
+    }
+
+    public void placeRecievedUnits(int territoryId){
+        int armyUnitNumber = 0;
+        int territoryNumber = _playerService.numberOfTerritories(_playerService.getCurrentPlayer().getId());
+
+        if (territoryNumber <= 9){
+            armyUnitNumber = 3;
+        }
+        else{
+            armyUnitNumber = territoryNumber / 3;
+        }
+
+        Territory placementTerritory = _mapService.findTerritory(territoryId);
+        if (placementTerritory.getOwnerId() == _playerService.getCurrentPlayer().getId()){
+            placementTerritory.getTerritoryArmy().addArmyUnits(ArmyUnitType.Infantry, armyUnitNumber);
+        }
+        else{
+            throw new Error("Select one of your own territories.");
+        }
     }
 }
