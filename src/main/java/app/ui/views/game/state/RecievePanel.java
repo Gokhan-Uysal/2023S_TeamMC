3068e7decddd220bdab9b3c12a37e15d6cefc1d4
EfeErtugErdem <efeerdem20@ku.edu.tr>
2023-05-17 17:20:35 +0300

    Started implementing the UI for the receive state.

--- src/main/java/app/ui/views/game/state/RecievePanel.java
+++ src/main/java/app/ui/views/game/state/RecievePanel.java
@@ -2,23 +2,39 @@ package app.ui.views.game.state;
 
 import app.ui.views.components.BaseStatePanel;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class RecievePanel extends BaseStatePanel {
+
+    private JLabel _recievedUnitNumberLabel;
+    private JLabel _selectedTerritoryLabel;
+    private JButton _nextPhaseButton;
 
     public RecievePanel() {
         super("Place your army units to map");
-        // TODO Auto-generated constructor stub
+        this.setLayout(new GridLayout(5,1));
+        initializeComponents();
+        addComponents();
     }
 
     @Override
     public void initializeComponents() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'initializeComponents'");
+        this._recievedUnitNumberLabel = new JLabel("You have recieved: ");
+        this._selectedTerritoryLabel = new JLabel("Please select the territory that you want to place your units.");
+        this._nextPhaseButton = new JButton("Go to the trading phase.");
+        setButtonActive(false);
     }
 
     @Override
     public void addComponents() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addComponents'");
+        this.add(_recievedUnitNumberLabel);
+        this.add(_selectedTerritoryLabel);
+        this.add(_nextPhaseButton);
+    }
+
+    public void setButtonActive(boolean isActive) {
+        _nextPhaseButton.setEnabled(isActive);
     }
 
 }
