6c0d0c68a1f93cbd669e1aa8d2cc7c36f38a8617
EfeErtugErdem <efeerdem20@ku.edu.tr>
2023-05-21 02:31:09 +0300

    Implemented the UI for the Replace state.

--- src/main/java/app/ui/controllers/game/state/ReplacePanelController.java
+++ src/main/java/app/ui/controllers/game/state/ReplacePanelController.java
@@ -1,4 +1,20 @@
 package app.ui.controllers.game.state;
 
-public class ReplacePanelController {
+import app.domain.models.game.map.Territory;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ReplacePanelController extends BaseStatePanelController implements ActionListener {
+
+
+    @Override
+    public void update(Territory message) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
--- src/main/java/app/ui/views/game/state/ReplacePanel.java
+++ src/main/java/app/ui/views/game/state/ReplacePanel.java
@@ -3,6 +3,7 @@ package app.ui.views.game.state;
 import app.ui.views.components.BaseStatePanel;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ReplacePanel extends BaseStatePanel {
     private JLabel _selectedTerritoryLabel;
@@ -13,16 +14,75 @@ public class ReplacePanel extends BaseStatePanel {
 
     public ReplacePanel() {
         super("Trade your army units.");
+        this.setLayout(new GridBagLayout());
+        initializeComponents();
+        addComponents();
+    }
 
+    public JButton getTradeUnitsButton(){
+        return this._tradeUnitsButton;
+    }
+
+    public JButton getNextPhaseButton(){
+        return this._nextPhaseButton;
+    }
+
+    public void addItemToComboBox(JComboBox<Integer> comboBox, int number){
+        comboBox.addItem(number);
     }
 
     @Override
     public void initializeComponents() {
+        this._selectedTerritoryLabel = new JLabel("Please select the territory you want to trade units on.");
+        this._tradeUnitsButton = new JButton("Trade units.");
+        this._nextPhaseButton = new JButton("Next phase.");
 
+        this._infantryAmountComboBox = new JComboBox<>();
+        this._cavalryAmountComboBox = new JComboBox<>();
     }
 
     @Override
     public void addComponents() {
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.weightx = 0.5;
+        constraints.weighty = 0.5;
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 4;
+        constraints.gridheight = 1;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        this.add(super._infoLabel, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 4;
+        this.add(_selectedTerritoryLabel, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        this.add(new JLabel("Infantry"), constraints);
+
+        constraints.gridx = 2;
+        this.add(new JLabel("Cavalry"), constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        this.add(_infantryAmountComboBox);
+
+        constraints.gridx = 2;
+        this.add(_cavalryAmountComboBox);
+
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridwidth = 1;
+        this.add(_tradeUnitsButton, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 4;
+        this.add(_nextPhaseButton);
 
     }
 }
