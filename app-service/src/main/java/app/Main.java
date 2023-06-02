731cb32f89300bf4d784701b94d96beaeea18a1e
mkilic20 <mkilic20@ku.edu.tr>
2023-06-02 22:43:26 +0300

    Player save & load tested

--- app-service/src/main/java/app/Main.java
+++ app-service/src/main/java/app/Main.java
@@ -2,25 +2,13 @@ package app;
 
 
 import app.common.errors.DbException;
-import app.domain.models.game.map.Territory;
-import app.domain.services.JsonSaveLoadService;
-import app.domain.services.map.MapReadService;
+import app.domain.repositories.PlayerRepository;
 import app.ui.views.menu.MainMenuFrame;
 
-import java.util.List;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) throws DbException {
-        MapReadService mrs = new MapReadService("app-service/src/main/java/app/__resource__/map.json");
-        mrs.buildGameMapData();
-        List<Territory> list = mrs.getGameMapTerritories();
-        System.out.println(list);
-        JsonSaveLoadService js = new JsonSaveLoadService();
-        js.saveMap(list);
-        List<Territory> newList = js.loadMap();
-        System.out.println(newList);
-        /*
-
         SwingUtilities.invokeLater(() -> {
             new MainMenuFrame();
             PlayerRepository pr = new PlayerRepository();
@@ -33,6 +21,5 @@ public class Main {
                 e.printStackTrace();
             }
         });
-        */
     }
 }
